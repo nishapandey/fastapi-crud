dd61fef3055589f95a5266333c9329c67624ea69
Nisha Pandey <npandey@redhat.com>
2025-09-09 20:24:01 -0400

    initial commit

--- app/main.py
+++ app/main.py
@@ -7,43 +7,22 @@ from psycopg2.extras import RealDictCursor
 from . import models
 from sqlalchemy.orm import Session
 from .database import engine, get_db
+from . import models
+from . import schemas
+from .schemas import Post, PostCreate, PostBase
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-while True:
-    try:
-        conn = psycopg2.connect(
-            dbname="testdb",
-            user="postgres",
-            password="123456",
-            host="localhost",
-            port="5432",
-            cursor_factory=RealDictCursor
-        )
-        cursor = conn.cursor()
-        print("Database connection was successful!")
-        break
-    except Exception as error:
-        print("Connecting to database failed: ", error)
-        time.sleep(2)
 
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    id: Optional[int] = None
-
-
-@app.get("/posts")
+@app.get("/posts", response_model=list[schemas.Post])
 def read_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def read_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
@@ -52,8 +31,8 @@ def read_post(id: int, db: Session = Depends(get_db)):
     return post
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
     db = next(get_db())
     db.add(new_post)
@@ -62,7 +41,7 @@ def create_post(post: Post):
     return new_post
 
 
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT,  response_class=Response)
 def delete_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
@@ -73,8 +52,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id}", status_code=status.HTTP_200_OK)
-def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", status_code=status.HTTP_200_OK, response_model=Post)
+def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     existing_post = db.query(models.Post).filter(models.Post.id == id).first()
     if not existing_post:
         raise HTTPException(
